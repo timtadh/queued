30d4445f4adfdece576cf8bb4b6db6b1fc4234ea
Tim Henderson <tim.tadh@gmail.com>
2015-04-10 17:48:46 -0400

    updated nets documentation
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- net/net.go
+++ net/net.go
@@ -1,39 +1,90 @@
-/*
-This package implements a TCP network service which allows clients to queue and
-deque data. The client has 2 verbs "ENQUE" and "DEQUE" and the server can send 3
-reponse status words "OK", "ERROR" and "ITEM".
-
-All messages have the following format:
-
-    COMMAND DATA
-
-Where DATA is Base64 encoded and COMMAND is a single ASCII word with no spaces.
-
-The client can send ENQUE or DEQUE at any time. If the queue is empty the server
-will respond with:
-
-    ERROR cXVldWUgaXMgZW1wdHk=
-
-Which decodes too:
-
-    ERROR queue is empty
-
-Otherwise it repondes with
-
-    ITEM XXXXXXXXXXXXXXX
-
-When enqueing data the client sends
-
-    ENQUE XXXXXXXXXXXXXXXXX
-
-The server will respond with
-
-    OK
-
-Unless there is some internal error.
-
-*/
-
+// This package implements a TCP network service which allows clients to queue
+// and deque data. The client has the following verbs
+//
+//  - ENQUE
+//  - DEQUE
+//  - HAS
+//  - SIZE
+//
+// the server can send the following reponse status words
+//
+//  - OK
+//  - ERROR
+//  - ITEM
+//  - TRUE
+//  - FALSE
+//  - SIZE
+//
+// All messages have the following format:
+//
+//     COMMAND DATA
+//
+// COMMAND is a single ASCII word with no spaces. DATA is the rest of the line.
+// DATA is optional. For ERROR, ENQUE and ITEM it is Base64 encoded. For SIZE
+// reponse it is ASCII.
+//
+// The client can send any command at any time. The server may at any command
+// respond with ERROR if there was a problem processing the command.
+//
+// ENQUE XXXXXXXXXXXXXXXX
+//
+//     XXXXXXXXXXXXXXXXXX should be base64 encoded data. If it is not the server
+//     will respond with and ERROR.
+//
+//     Otherwise the server will repond
+//
+//          OK
+//
+//     The server should always respond with OK if the command is properly
+//     formated. If it does not that means there is an internal error in the
+//     server.
+//
+// DEQUE
+//
+//     If the queue is empty the server will respond with:
+//
+//         ERROR cXVldWUgaXMgZW1wdHk=
+//
+//     Which decodes too:
+//
+//         ERROR queue is empty
+//
+//     Otherwise it repondes with
+//
+//         ITEM XXXXXXXXXXXXXXX
+//
+// HAS XXXXXXXXXXXXXXXXXXXXXXXXXXX
+//
+//      HAS checks for the existence of an item. It doesn't send the item but
+//      rather the base64 encoded sha265sum. Example for producing this (in
+//      Python for simplicity)
+//
+//         >>> import hashlib
+//         >>> hashlib.sha256('item').digest().encode('base64').strip()
+//         'SjPqzV+mXysuKHHNExKGtTxBWxMWZtcRc7tuP+WTYbM='
+//
+//      The client would send
+//
+//          HAS SjPqzV+mXysuKHHNExKGtTxBWxMWZtcRc7tuP+WTYbM=
+//
+//      The server would reponse either
+//
+//          TRUE
+//
+//      or
+//
+//          FALSE
+//
+//
+// SIZE
+//
+//      The server response with the size of the queue encoded as a base10 ascii
+//      number. eg.
+//
+//          SIZE 9231
+//
+//      For a queue that is 9,231 items long.
+//
 package net
 
 /* queued
